e57a40338b5a1bfb83a56514418d1c28e3d00326
Franchesco007 <franchesco1510@gmail.com>
2023-12-22 04:41:09 -0500

    prueba

--- start.sh
+++ start.sh
@@ -15,8 +15,15 @@ npm install
 # Go back to the main directory
 cd ..
 
+# Set your database connection properties (replace placeholders with actual values)
+export SPRING_DATASOURCE_URL=jdbc:mysql://localhost:3306/notesdb
+export SPRING_DATASOURCE_USERNAME=root
+export SPRING_DATASOURCE_PASSWORD=1234
+
 # Start the backend (make sure your database server is running)
 cd backend
+
+# Start the backend
 ./mvnw spring-boot:run &
 
 # Go back to the main directory
